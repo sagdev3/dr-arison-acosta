b10402564caabbe366120917666a6469e0943cd7
sagdev3 <sagdevweb@gmail.com>
2022-10-05 16:42:54 -0500

    cambios menu movil y modo oscuro movil

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('#menu');
 const body = document.querySelector('body');
 const moonBotton = document.querySelector('#moon-botton');
 const hlinks = document.querySelectorAll('.hlink');
-
+const oscuroMovil = document.querySelector('#modo-oscuro-movil');
 hamburgerMenu.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
@@ -14,5 +14,8 @@ hlinks.forEach(link => {
   });
 });
 moonBotton.addEventListener('click', () => {
+  body.classList.toggle('dark');
+});
+oscuroMovil.addEventListener('click', () => {
   body.classList.toggle('dark');
 });
