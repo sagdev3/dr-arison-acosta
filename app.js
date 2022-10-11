ef307fe69dd36e127db67c690121d76636a30531
sagdev3 <sagdevweb@gmail.com>
2022-10-11 12:57:36 -0500

    actualizacion de la pagina

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const body = document.querySelector('body');
 const moonBotton = document.querySelector('#moon-botton');
 const hlinks = document.querySelectorAll('.hlink');
 const oscuroMovil = document.querySelector('#modo-oscuro-movil');
+
 hamburgerMenu.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
@@ -18,4 +19,21 @@ moonBotton.addEventListener('click', () => {
 });
 oscuroMovil.addEventListener('click', () => {
   body.classList.toggle('dark');
+});
+
+var openButton = document.getElementById('open');
+var dialog = document.getElementById('dialog');
+var closeButton = document.getElementById('close');
+var overlay = document.getElementById('overlay');
+
+// show the overlay and the dialog
+openButton.addEventListener('click', function () {
+  dialog.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+});
+
+// hide the overlay and the dialog
+closeButton.addEventListener('click', function () {
+  dialog.classList.add('hidden');
+  overlay.classList.add('hidden');
 });
